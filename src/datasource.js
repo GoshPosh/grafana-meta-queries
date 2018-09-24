77114ea00edc45af0572c882dd19ddb30a50b955
gaurav <gaurav@poshmark.com>
2018-09-23 23:37:01 -0700

    moving to functional paradigm so that recursion is possible

--- src/datasource.js
+++ src/datasource.js
@@ -125,11 +125,13 @@ function (angular, _, dateMath, moment) {
                     })
                   }
               });
-              return [{
-                "target": outputMetricName,
-                 "datapoints": datapoints,
-                  "hide" : target.hide
-              }];
+                return {
+                    data: [{
+                        "target": outputMetricName,
+                        "datapoints": datapoints,
+                        "hide": target.hide
+                    }]
+                };
                 // var fromMs = formatTimestamp(from);
                 // metrics.forEach(function (metric) {
                 //     if (!_.isEmpty(metric.datapoints[0]) && metric.datapoints[0][1] < fromMs) {
@@ -142,62 +144,8 @@ function (angular, _, dateMath, moment) {
 
       }
       else if (target.queryType === 'MovingAverage') {
-          var periodsToShift = target.periods;
-          var query = target.query;
-          var metric = target.metric;
 
-
-
-          options.range.from._d = dateToMoment(options.range.from, false).subtract(periodsToShift-1,'days').toDate();
-
-          var metaTarget = angular.copy(targetsByRefId[query]);
-          metaTarget.hide = false;
-          options.targets = [metaTarget]
-
-          promise = datasourceSrv.get(options.targets[0].datasource).then(function(ds) {
-              return $q.all([promisesByRefId[query],ds.query(options)]).then(function(results) {
-//              when plotting moving average the first data point does not show up. That leaves with broken graph in the beginning.
-//              We are calculating actualFrom from the first timestamp from the original promise query and then pushing datapoints whose timestamps that are greater or equal to actualFrom
-              if(results[0]['data'][0]['datapoints'][0]==undefined) {
-                var actualFrom = null
-              }else{
-                var actualFrom = results[0]['data'][0]['datapoints'][0][1]
-              }
-                  var datapoints = []
-                  var data = results[1].data;
-                  data.forEach(function (datum) {
-                      if(datum.target===metric){
-                          var datapointByTime = {};
-                          datum.datapoints.forEach(function (datapoint) {
-                              datapointByTime[datapoint[1]] = datapoint[0];
-
-                              var metricSum = 0;
-                              for(var count = 0; count < periodsToShift; count++) {
-                                  var targetDate = dateToMoment(new Date(datapoint[1]),false).subtract(count,'days').toDate().getTime()
-                                  metricSum += datapointByTime[targetDate] || 0
-                              }
-
-                              if(actualFrom && datapoint[1]>=actualFrom){
-                                  datapoints.push([metricSum/periodsToShift,datapoint[1]])
-                              }
-                          })
-                      }
-                  });
-                  return [{
-                      "target": outputMetricName,
-                      "datapoints": datapoints,
-                      "hide" : target.hide
-                  }];
-                  // var fromMs = formatTimestamp(from);
-                  // metrics.forEach(function (metric) {
-                  //     if (!_.isEmpty(metric.datapoints[0]) && metric.datapoints[0][1] < fromMs) {
-                  //         metric.datapoints[0][1] = fromMs;
-                  //     }
-                  // });
-
-              });
-          });
-
+          promise = moving_average(target, options, targetsByRefId, datasourceSrv, outputMetricName, promisesByRefId)
 
       }
       else if (target.queryType === 'Arithmetic') {
@@ -244,22 +192,19 @@ function (angular, _, dateMath, moment) {
               });
 
 
-              return [{
-                  "target": outputMetricName,
-                  "datapoints": datapoints,
-                  "hide" : target.hide
-              }];
+              return {
+                  data: [{
+                      "target": outputMetricName,
+                      "datapoints": datapoints,
+                      "hide" : target.hide
+                  }]
+              };
           })
       }
 
 
-      var dataWrappedPromise =   promise.then(function (metrics) {
-          return {
-              data: metrics
-          }
-      });
-      promisesByRefId[target.refId] = dataWrappedPromise;
-      metaQueryPromises.push(dataWrappedPromise);
+      promisesByRefId[target.refId] = promise;
+      metaQueryPromises.push(promise);
       targetsByRefId[target.refId]= target;
 
       });
@@ -280,6 +225,68 @@ function (angular, _, dateMath, moment) {
     }
 
 
+
+    function moving_average(target, options, targetsByRefId, datasourceSrv, outputMetricName, promisesByRefId){
+        var promise = null;
+          var periodsToShift = target.periods;
+        var query = target.query;
+        var metric = target.metric;
+
+
+
+        options.range.from._d = dateToMoment(options.range.from, false).subtract(periodsToShift-1,'days').toDate();
+
+        var metaTarget = angular.copy(targetsByRefId[query]);
+        metaTarget.hide = false;
+        options.targets = [metaTarget]
+
+        promise = datasourceSrv.get(options.targets[0].datasource).then(function(ds) {
+            return $q.all([promisesByRefId[query],ds.query(options)]).then(function(results) {
+//              when plotting moving average the first data point does not show up. That leaves with broken graph in the beginning.
+//              We are calculating actualFrom from the first timestamp from the original promise query and then pushing datapoints whose timestamps that are greater or equal to actualFrom
+                if(results[0]['data'][0]['datapoints'][0]==undefined) {
+                    var actualFrom = null
+                }else{
+                    var actualFrom = results[0]['data'][0]['datapoints'][0][1]
+                }
+                var datapoints = []
+                var data = results[1].data;
+                data.forEach(function (datum) {
+                    if(datum.target===metric){
+                        var datapointByTime = {};
+                        datum.datapoints.forEach(function (datapoint) {
+                            datapointByTime[datapoint[1]] = datapoint[0];
+
+                            var metricSum = 0;
+                            for(var count = 0; count < periodsToShift; count++) {
+                                var targetDate = dateToMoment(new Date(datapoint[1]),false).subtract(count,'days').toDate().getTime()
+                                metricSum += datapointByTime[targetDate] || 0
+                            }
+
+                            if(actualFrom && datapoint[1]>=actualFrom){
+                                datapoints.push([metricSum/periodsToShift,datapoint[1]])
+                            }
+                        })
+                    }
+                });
+                return {
+                    data: [{
+                        "target": outputMetricName,
+                        "datapoints": datapoints,
+                        "hide" : target.hide
+                    }]
+                };
+                // var fromMs = formatTimestamp(from);
+                // metrics.forEach(function (metric) {
+                //     if (!_.isEmpty(metric.datapoints[0]) && metric.datapoints[0][1] < fromMs) {
+                //         metric.datapoints[0][1] = fromMs;
+                //     }
+                // });
+
+            });
+        });
+        return promise;
+    }
 
 
   }
